5f9e6c4148b8e532715734d211bd53ab717ee18c
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-09-04 09:44:50 -0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-codefresh auth create-context --api-key "$1"
+codefresh auth create-context --api-key $1
 helm plugin install https://github.com/chartmuseum/helm-push
 helm repo add default-repo $2
 helm push ./chart default-repo

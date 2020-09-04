4414e0c4aca3e8abd403ce71b07c359d99fc31ca
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-09-04 11:01:40 -0500

    rework token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
 helm plugin install https://github.com/chartmuseum/helm-push
-codefresh auth create-context --api-key $1
+
 helm repo add default-repo $2
-helm push ./chart default-repo
+helm push ./chart default-repo --access-token $1
 ls
+cat ~/.cfconfig
 echo "Hello $1 $2"
 time=$(date)
 echo "::set-output name=time::$time"

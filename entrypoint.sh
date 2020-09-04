d0afbf7860d9faea60014a60006bcc20d020cf35
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-09-04 02:00:59 -0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ helm plugin install https://github.com/chartmuseum/helm-push
 helm repo add default-repo $2
 helm push ./chart default-repo
 ls
-echo "Hello $1"
+echo "Hello $1 $2"
 time=$(date)
 echo "::set-output name=time::$time"

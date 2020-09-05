bda94da1c120c77ace987797f308e89b6c7dd87d
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-09-05 11:52:57 -0500

    Rework entrypoint logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,42 +11,40 @@ if [ -z "$CHARTMUSEUM_URL" ]; then
   exit 1
 fi
 
+if [ -z "$CHARTMUSEUM_ACCESS_TOKEN" ] && [ [ -z "$CHARTMUSEUM_USERNAME" ] || [ -z "$CHARTMUSEUM_PASSWORD" ] ]
+  echo "Credentials are required, but none defined."
+  exit 1
+fi
+
 if [ -z "$FORCE" ]; then
   FORCE=""
 elif [ "$FORCE" == "1" ] || [ "$FORCE" == "True" ] || [ "$FORCE" == "TRUE" ] || [ "$FORCE" == "true" ]; then
   FORCE="-f"
 fi
 
-if [ -z "$CHARTMUSEUM_ACCESS_TOKEN" ] && [ [ -z "$CHARTMUSEUM_USERNAME" ] || [ -z "$CHARTMUSEUM_PASSWORD" ] ]
-  echo "Credentials are required, but none defined."
-  exit 1
-fi
-
 if [ "$CHARTMUSEUM_ACCESS_TOKEN" ]; then
   echo "Access token is defined, using bearer auth."
-
-  cd ${CHART_FOLDER}
-  helm inspect chart .
-  helm lint .
-  helm push . chart-repo --access-token ${CHARTMUSEUM_ACCESS_TOKEN} ${FORCE}
+  CHARTMUSEUM_ACCESS_TOKEN="--access-token ${CHARTMUSEUM_ACCESS_TOKEN}"
 fi
 
-if [ -z "$CHARTMUSEUM_ACCESS_TOKEN" ]; then
-  echo "Access token is not defined, using basic auth."
 
-  if [ -z "$CHARTMUSEUM_USERNAME" ]; then
-    echo "Username is required but not defined."
-    exit 1
-  fi
+if [ "$CHARTMUSEUM_USERNAME" ]; then
+  echo "Username is defined, using as parameter."
+  CHARTMUSEUM_USERNAME="--username ${CHARTMUSEUM_USERNAME}"
+fi
 
-  if [ -z "$CHARTMUSEUM_PASSWORD" ]; then
-    echo "Password is required but not defined."
-    exit 1
-  fi
+if [ "$CHARTMUSEUM_PASSWORD" ]; then
+  echo "Password is defined, using as parameter."
+  CHARTMUSEUM_PASSWORD="--password ${CHARTMUSEUM_PASSWORD}"
+fi
 
-  cd ${CHART_FOLDER}
-  helm inspect chart .
-  helm lint .
-  helm push . ${CHARTMUSEUM_URL} --username ${CHARTMUSEUM_USERNAME} --password ${CHARTMUSEUM_PASSWORD} ${FORCE}
+if [ "$CHARTMUSEUM_VERSION" ]; then
+  echo "Password is defined, using as parameter."
+  CHARTMUSEUM_VERSION="--password ${CHARTMUSEUM_VERSION}"
+fi
+
+cd ${CHART_FOLDER}
+helm inspect chart .
+helm lint .
+helm push . ${CHARTMUSEUM_URL} ${CHARTMUSEUM_USERNAME} ${CHARTMUSEUM_PASSWORD} ${CHARTMUSEUM_ACCESS_TOKEN} ${CHARTMUSEUM_VERSION} ${FORCE}
   
-fi
